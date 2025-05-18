a0f88ccf207f277b6fa7cc7d629aa9173842669c
anderskambenders <a.zetsmanis@mail.ru>
2025-05-18 15:13:00 +0300

    feat: add attack req

--- src/controller/response.ts
+++ src/controller/response.ts
@@ -5,6 +5,7 @@ import {
   REQUEST_TYPE,
   WebsocketResponse,
   RESPONSE_TYPE,
+  PlayerId,
 } from '../types';
 import getRegistrationResp from './registration';
 import updateRoom from './updateRoom';
@@ -51,6 +52,52 @@ const getResponse = (request: WebsocketRequest, name?: string) => {
             wsConnections[index].ws.send(JSON.stringify(resp));
           });
         }
+      },
+      [REQUEST_TYPE.addShips]: ({ request }: { request: WebsocketRequest }) => {
+        const data = JSON.parse(request.data);
+        const { gameId, ships, indexPlayer: playerId } = data;
+        if (Games.shouldStart(gameId, ships, playerId)) {
+          const players = Games.getGamePlayers(gameId);
+          players.forEach((player) => {
+            const response: WebsocketResponse = {
+              type: RESPONSE_TYPE.startGame,
+              data: JSON.stringify({
+                ships: player.ships,
+                currentPlayerIndex: player.index,
+              }),
+              id: 0,
+            };
+            wsConnections[
+              getWsEntryIndexByKey('userName', player.name)
+            ].ws.send(JSON.stringify(response));
+          });
+          Games.setTurn(gameId);
+          const turn = Games.getTurn(gameId);
+          const response: WebsocketResponse = {
+            type: RESPONSE_TYPE.turn,
+            data: JSON.stringify({ currentPlayer: turn }),
+            id: 0,
+          };
+          const playersAmount = Games.getGamePlayers(gameId);
+          playersAmount.forEach((player) => {
+            const index = getWsEntryIndexByKey('userName', player.name);
+            wsConnections[index].ws.send(JSON.stringify(response));
+          });
+        }
+      },
+      [REQUEST_TYPE.attack]: ({ request }: { request: WebsocketRequest }) => {
+        const data = JSON.parse(request.data);
+        const {
+          gameId,
+          x,
+          y,
+          indexPlayer,
+        }: { gameId: string; x: number; y: number; indexPlayer: PlayerId } =
+          data;
+
+        if (data.indexPlayer !== Games.getTurn(gameId)) return;
+
+        handleAttack(gameId, indexPlayer, x, y);
       },
     };
 
